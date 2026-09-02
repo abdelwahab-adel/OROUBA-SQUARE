3dadac2faa333601e354c1b9a1e53f6d89df1fd7
Abdelwahab <132628897+abdelwahab-adel@users.noreply.github.com>
2026-09-02 08:50:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -404,7 +404,7 @@ function openSrv(key) {
   Utils.$('#sd-long').textContent = s.long;
   Utils.$('#sd-feats').innerHTML = s.feats.map(f => `
     <div class="sd-feature is-visible">
-      <div class="sd-feature__icon"><svg width="22" height="22" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"/></svg></div>
+      
       <div><div class="sd-feature__title">${f.t}</div><div class="sd-feature__desc">${f.d}</div></div>
     </div>`).join('');
   Utils.$('#sd-others').innerHTML = Object.values(SRV).filter(x => x.id !== key).map(x => `
